be9993c2ffe0b706aa52718cd3bed8069faebc82
Pablo Baez <pablobaez2005@gmail.com>
2024-11-21 13:29:55 -0500

    Conexion con Railway

--- src/config/database.js
+++ src/config/database.js
@@ -3,10 +3,23 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-  host: process.env.DB_HOST,
-  dialect: 'postgres',
-  logging: (msg) => console.log(`Sequelize: ${msg}`)
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME, 
+  process.env.DB_USER, 
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false, // Importante para Railway
+      },
+    },
+    logging: (msg) => console.log(`Sequelize: ${msg}`),
+  }
+);
+
 
 export default sequelize;
