fbc6afe671cfb85845830856757df07ae652b18f
Pablo Baez <pablobaez2005@gmail.com>
2024-11-21 18:36:40 -0500

    prueba con vercel3

--- src/config/database.js
+++ src/config/database.js
@@ -21,5 +21,12 @@ const sequelize = new Sequelize(
   }
 );
 
+sequelize.authenticate()
+  .then(() => {
+    console.log('Conexión a la base de datos establecida con éxito.');
+  })
+  .catch(err => {
+    console.error('No se pudo conectar a la base de datos:', err);
+  });
 
 export default sequelize;
